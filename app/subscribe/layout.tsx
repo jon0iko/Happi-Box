febed5d05720f9f9292dabc053de91990b2bee17
jon0iko <price6967@gmail.com>
2024-09-23 00:16:56 +0600

    nextjs update

--- app/subscribe/layout.tsx
+++ app/subscribe/layout.tsx
@@ -4,17 +4,14 @@ import Nav from '../items/navbar'
 import footer from './footer'
 
 
-export default function RootLayout({
-    children,
-  }: {
-    children: React.ReactNode
-  }) {
+export default function RootLayout({children,}: {children: React.ReactNode}) {
     return (
       <html lang="en">
         <body>
           <ChakraProvider>
             <Nav />
             {children}
+            <footer />
           </ChakraProvider>
         </body>
       </html>
