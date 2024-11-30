9b574929a28d4da1bbcfd2f3d843fbb7d5cfd01a
jon0iko <price6967@gmail.com>
2024-12-01 00:45:13 +0600

    Change image

--- app/page.tsx
+++ app/page.tsx
@@ -93,11 +93,11 @@ export default function Home() {
         </div>
       </div>
 
-      <div className='bg-[url("/demoo.jpg")] bg-cover bg-center'>
-  <div className="text-white body-font px-4 sm:px-6 lg:px-8 xl:px-28 py-8 sm:py-12 lg:py-16">
+      <div className='bg-[url("/Demo.jpg")] bg-cover bg-center'>
+  <div className="text-black body-font px-4 sm:px-6 lg:px-8 xl:px-28 py-8 sm:py-12 lg:py-16">
     <div className="container mx-auto flex flex-col lg:flex-row items-center">
       <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
-        <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-white">
+        <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-black">
           Why Choose Bhojon Box{" "}
         </h1>
         <p className="mb-8 leading-relaxed">
